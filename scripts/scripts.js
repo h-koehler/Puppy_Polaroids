2c48b5ebdb6798ca5ff7e2050ad84ee6aa54afa2
ethan <erh3429@rit.edu>
2023-08-15 13:08:57 -0400

    Added new polaroid forEach, fixed spelling of doog

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,7 @@
 const bkgd = document.getElementById("noMove");
 
+
+
 let labModal = document.getElementById("labModal");
 let berneseModal = document.getElementById("berneseModal");
 let shibaModal = document.getElementById("shibaModal");
@@ -7,29 +9,28 @@ let goldenModal = document.getElementById("goldenModal");
 let lapphundModal = document.getElementById("lapphundModal");
 let samoyedModal = document.getElementById("samoyedModal");
 
-let labBtn = document.getElementById("labBtn");
-let berneseBtn = document.getElementById("berneseBtn");
-let shibaBtn = document.getElementById("shibaBtn");
-let goldenBtn = document.getElementById("goldenBtn");
-let lapphundBtn = document.getElementById("lapphundBtn");
-let samoyedBtn = document.getElementById("samoyedBtn");
-
 let span = document.getElementsByClassName("close")[0];
+let modalArr = [labModal, berneseModal, shibaModal, lapphundModal, samoyedModal];
 
+let polaroids = document.querySelectorAll('.polaroid');
+let counter = 0;
 
-labBtn.onclick = function () {
-    labModal.style.display = "block";
+polaroids.forEach(function (polaroid, counter) {
+    polaroid.addEventListener('click', function() {
+    modalArr[counter].style.display = "block";
     bkgd.style.overflow = "hidden";
-}
+    counter++;
+    });
+});
 
-span.onclick = function () {
-    labModal.style.display = "none";
+span.forEach() closeModalSpan(modal) {
+    modal.style.display = "none";
     bkgd.style.overflow = "visible";
 }
 
-window.onclick = function (event) {
-    if (event.target == labModal) {
-        labModal.style.display = "none";
+function closeModalWindow(event, modal) {
+    if (event.target == modal) {
+        modal.style.display = "none";
         bkgd.style.overflow = "visible";
     }
 }
