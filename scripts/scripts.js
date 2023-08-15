42b854499aed93ce4fbd2722646860c838a0491e
hkoehler20 <hannahkoehler19@gmail.com>
2023-08-14 23:14:46 -0400

    Organized images and updated modal appearance.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,4 @@
+const bkgd = document.getElementById("noMove");
 var modal = document.getElementById("labModal");
 console.log(document.getElementById("labBtn"));
 var btn = document.getElementById("labBtn");
@@ -6,14 +7,17 @@ var span = document.getElementsByClassName("close")[0];
 
 btn.onclick = function () {
     modal.style.display = "block";
+    bkgd.style.overflow = "hidden";
 }
 
 span.onclick = function () {
     modal.style.display = "none";
+    bkgd.style.overflow = "visible";
 }
 
 window.onclick = function (event) {
     if (event.target == modal) {
         modal.style.display = "none";
+        bkgd.style.overflow = "visible";
     }
 }
