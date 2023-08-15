4e3c0089aa7fcd6df414a6794a316358cc51373f
hkoehler20 <hannahkoehler19@gmail.com>
2023-08-15 11:40:45 -0400

    Tried adding buttons & modals for other breeds.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,22 +1,35 @@
 const bkgd = document.getElementById("noMove");
-let modal = document.getElementById("labModal");
-let btn = document.getElementById("labBtn");
+
+let labModal = document.getElementById("labModal");
+let berneseModal = document.getElementById("berneseModal");
+let shibaModal = document.getElementById("shibaModal");
+let goldenModal = document.getElementById("goldenModal");
+let lapphundModal = document.getElementById("lapphundModal");
+let samoyedModal = document.getElementById("samoyedModal");
+
+let labBtn = document.getElementById("labBtn");
+let berneseBtn = document.getElementById("berneseBtn");
+let shibaBtn = document.getElementById("shibaBtn");
+let goldenBtn = document.getElementById("goldenBtn");
+let lapphundBtn = document.getElementById("lapphundBtn");
+let samoyedBtn = document.getElementById("samoyedBtn");
+
 let span = document.getElementsByClassName("close")[0];
 
 
-btn.onclick = function () {
-    modal.style.display = "block";
+labBtn.onclick = function () {
+    labModal.style.display = "block";
     bkgd.style.overflow = "hidden";
 }
 
 span.onclick = function () {
-    modal.style.display = "none";
+    labModal.style.display = "none";
     bkgd.style.overflow = "visible";
 }
 
 window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+    if (event.target == labModal) {
+        labModal.style.display = "none";
         bkgd.style.overflow = "visible";
     }
 }
