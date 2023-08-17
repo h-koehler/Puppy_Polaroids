e352caae5ffdaa79e9a30a2eb548141de2e21303
hkoehler20 <hannahkoehler19@gmail.com>
2023-08-16 22:29:34 -0400

    BMD & Shiba modals updated. Script now allows scrolling after clicking off window.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -9,21 +9,22 @@ polaroids.forEach(function (polaroid) {
         let modalId = polaroid.id + "Modal"; // gets the ID of the modal
         let modal = document.getElementById(modalId); // gets the modal using the id
         currentModal = modal; // updates the current modal
-        modal.style.display = "block"; // applies changes to foreground & background
-        bkgd.style.overflow = "hidden";
+        modal.style.display = "block"; // opens modal
+        bkgd.style.overflow = "hidden"; // freezes background
     });
 });
 
 spans.forEach(function (span) {
     span.addEventListener('click', function () {
         let modal = this.parentElement.parentElement; // gets the modal by looking at the Grandparent (TM)
-        modal.style.display = "none"; // applies changes to foreground & background
-        bkgd.style.overflow = "visible";
+        modal.style.display = "none"; // closes modal
+        bkgd.style.overflow = "visible"; // unfrezes background
     });
 });
 
 window.onclick = function (event) { // allows user to click outside modal to close it
     if (event.target == currentModal) {
         currentModal.style.display = "none";
+        bkgd.style.overflow = "visible";
     }
 }
